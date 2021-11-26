ee25d51b2888f5071b207056f05bdbc2e96e53fe
Arya Zafri <aryaxszafrie@gmail.com>
2021-11-26 10:25:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/to_dolistapps/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.example.to_dolistapps;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialogInterface);
+}
